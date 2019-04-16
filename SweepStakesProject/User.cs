81d4a6d5dcbcd6e3dfcacf52e45b6c919dde8d6a
DejaMeshae <daemeshae@yahoo.com>
2019-04-16 10:24:54 -0500

    Added Iuser interface

---
+++ SweepStakesProject/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakesProject
+{
+    public interface IUser
+    {
+        int FirstName { get; set; }
+        int lastName { get; set; }
+        int emailAddress { get; set; }
+        int registrationNumber { get; set; }
+    }
+}
